f791fc904aa24058feb3aa1d7f81adc1ae08ca88
Avec112 <avec112@gmail.com>
2022-09-16 09:14:37 +0200

    Using XYZ source

--- src/main/java/com/github/views/map/MapView.java
+++ src/main/java/com/github/views/map/MapView.java
@@ -4,6 +4,8 @@ import com.github.views.MainLayout;
 import com.vaadin.flow.component.map.Map;
 import com.vaadin.flow.component.map.configuration.Coordinate;
 import com.vaadin.flow.component.map.configuration.View;
+import com.vaadin.flow.component.map.configuration.layer.TileLayer;
+import com.vaadin.flow.component.map.configuration.source.XYZSource;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -14,15 +16,28 @@ import com.vaadin.flow.router.RouteAlias;
 @RouteAlias(value = "", layout = MainLayout.class)
 public class MapView extends VerticalLayout {
 
-    private Map map = new Map();
+    private final Map map = new Map();
 
     public MapView() {
         setSizeFull();
         setPadding(false);
+
+        setupXyzSource();
+
         map.getElement().setAttribute("theme", "borderless");
         View view = map.getView();
         view.setCenter(new Coordinate(10.0, 55.0));
         view.setZoom(4);
         addAndExpand(map);
+    }
+
+    private void setupXyzSource() {
+        XYZSource.Options sourceOptions = new XYZSource.Options();
+        sourceOptions.setUrl("https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+        sourceOptions.setAttributionsCollapsible(false);
+        XYZSource source = new XYZSource(sourceOptions);
+        TileLayer tileLayer = new TileLayer();
+        tileLayer.setSource(source);
+        map.setBackgroundLayer(tileLayer);
     }
 }
